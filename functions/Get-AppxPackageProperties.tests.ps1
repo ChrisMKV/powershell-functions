335c806c54ff11646e7740f7d93a85d7c5cf5eb7
ChrisMKV <41515846+ChrisMKV@users.noreply.github.com>
2024-01-12 20:57:19 +0100

    Improve tests

--- functions/Get-AppxPackageProperties.tests.ps1
+++ functions/Get-AppxPackageProperties.tests.ps1
@@ -1,8 +1,11 @@
 BeforeAll { . $PSScriptRoot/Get-AppxPackageProperties.ps1 }
 Describe 'Get-AppxPackageProperties' {
-	Context 'Fails parameter validation' {
+	Context 'Parameter validation' {
 		It 'should fail parameter validation if -Path is not a valid path' {
 			{ Get-AppxPackageProperties -Path 'WRONG' } | Should -Throw
+		}
+		It 'should fail parameter validation if -Path is not an .msix file' {
+			{ Get-AppxPackageProperties -Path $PSCommandPath } | Should -Throw
 		}
 	}
 }
